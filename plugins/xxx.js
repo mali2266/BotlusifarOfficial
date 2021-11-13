bf664e4b8b3f2c88a281ba1f522c2d76dc1ec258
kavishka <91202334+kavishkaya@users.noreply.github.com>
2021-11-13 16:20:36 +0530

    .video self mode Avalable

--- plugins/xxx.js
+++ plugins/xxx.js
@@ -28,3 +28,22 @@ const tk = Config.WORKTYPE == 'public' ? false : true
         )
       },
     )
+    lusifar.addCommand({ pattern: 'video ?(.*)', fromMe: true, deleteCommand: false, desc: "Download videos",  deleteCommand: false}, async (message, match) => {
+        const linkk = match[1]
+        if (!linkk) return await message.client.sendMessage(message.jid,YT_NEED,MessageType.text)
+        await message.client.sendMessage(message.jid,DWLOAD_VID,MessageType.text ,  {quoted: message.data});
+        await axios
+          .get(`https://hardianto-chan.herokuapp.com/api/download/ytdownload?url=${linkk}&apikey=hardianto`)
+          .then(async (response) => {
+            const {
+              link,
+            } = response.data.result
+            const videoBuffer = await axios.get(link, {responseType: 'arraybuffer'})
+            await message.client.sendMessage(message.jid,YTV_UP,MessageType.text ,  {quoted: message.data});
+            await message.client.sendMessage(message.jid,Buffer.from(videoBuffer.data), MessageType.video,  {quoted: message.data} ,{mimetype: Mimetype.mp4, ptt: false})
+        })
+        .catch(
+          async (err) => await message.client.sendMessage(message.jid,NO_RESULT,MessageType.text, {quoted: message.data}),
+        )
+      },
+    )
